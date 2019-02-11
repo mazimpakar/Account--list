0f63f152a82ce60fce779b7bcdd0a80a7c08ead7
mazimpakar <rosemazimpaka2@gmail.com>
2019-02-11 17:17:58 +0200

    add test to save

--- account-test.py
+++ account-test.py
@@ -1,5 +1,5 @@
 import unittest # Importing the unittest module
-from account import accountt # Importing the account class
+from user import Account # Importing the account class
 
 class TestAccount(unittest.TestCase):
 
@@ -8,23 +8,36 @@ class TestAccount(unittest.TestCase):
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
-    'def setUp(self):
+        '''
+
+    def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_account = account("facebook","mazimpaka","0786950337ro") # create account object
-''
+        self.new_account = Account("facebook","mazimpaka","0786950337ro") # create account object
+        ''
 
-def test_init(self):
+    def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_account.first_name,"facebook")
-        self.assertEqual(self.new_account.last_name,"mazimpakar")
+        self.assertEqual(self.new_account.account_name,"facebook")
+        self.assertEqual(self.new_account.user_name,"mazimpaka")
         self.assertEqual(self.new_account.password,"0786950337ro")
         
 
 
 if __name__ == '__main__':
     unittest.main()
+
+    def test_save_account(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list
+        '''
+        self.new_account.save_account() # saving the new account
+        self.assertEqual(len(Account.account_list),1)
+
+if __name__ ==  '__main__':
+    unittest.main()  
